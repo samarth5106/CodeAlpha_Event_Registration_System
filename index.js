7ff1b76259b7dfe09b7e01249e1480800f661445
Samarth Manish Rathod <samarth9578@gmail.com>
2026-05-27 22:32:23 +0530

    feat: configure main server entry point with core parsing middleware

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+
+const app = express();
+const PORT = process.env.PORT || 8002;
+
+connectToMongoDB(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB Connected Successfully for Event Reg App"))
+    .catch((err) => console.error("MongoDB Connection Error:", err));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => console.log(`Server started and tracking on PORT: ${PORT}`));
